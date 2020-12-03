b37c28a052b3bd16f3703ee13ae9f08dbfc26486
sotrteacher <lamberto9@gmail.com>
2020-12-03 11:00:08 -0600

    2020.12.03

---
+++ prog29_HerenciaSimple/include/CCuenta.h
@@ -0,0 +1,15 @@
+#ifndef CCUENTA_H
+#define CCUENTA_H
+
+
+class CCuenta
+{
+    public:
+        CCuenta();
+
+    protected:
+
+    private:
+};
+
+#endif // CCUENTA_H
---
+++ prog29_HerenciaSimple/src/CCuenta.cpp
@@ -0,0 +1,6 @@
+#include "CCuenta.h"
+
+CCuenta::CCuenta()
+{
+    //ctor
+}
