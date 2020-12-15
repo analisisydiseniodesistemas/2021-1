4c04315892f09759822a2c73a98209f13172b9ee
sotrteacher <lamberto9@gmail.com>
2020-12-15 09:51:42 -0600

    2020.12.15

--- prog31_Polimorfismo/cuenta_corriente+.h
+++ prog31_Polimorfismo/cuenta_corriente+.h
@@ -1,4 +1,4 @@
-// cuenta_corriente_plus.h - Clase CCuentaCorrienteConIn
+// cuenta_corriente+.h - Clase CCuentaCorrienteConIn
 #if !defined( _CUENTA_CORRIENTECOIN_H_ )
 #define _CUENTA_CORRIENTECOIN_H_
 #include "cuenta_corriente.h"
@@ -8,9 +8,10 @@ class CCuentaCorrienteConIn : public CCuentaCorriente
   // Métodos
   public:
     CCuentaCorrienteConIn(std::string nom = "sin nombre",
-     std::string cue = "0000", double sal = 0.0, double tipo = 0.0,
-     double imptrans = 0.0, int transex = 0);
+        std::string cue = "0000", double sal = 0.0, double tipo = 0.0,
+        double imptrans = 0.0, int transex = 0);
     double intereses();
+    CCuentaCorrienteConIn *clonar();
 };
 
-#endif // _CUENTA_CORRIENTECOIN_H_
+#endif // _CUENTA_CORRIENTECOIN_H_ 
